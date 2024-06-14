df63a256e0571ec979d40f16f9a40e944ab8466e
bowling00 <zhangbaolin.work@foxmail.com>
2024-06-15 00:10:02 +0800

    feat: change image test

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE_FAVICON = "/favicon.png";
 export const SITE_LOGO = "/avatar.png";
-export const SITE_TITLE = "Mia";
+export const SITE_TITLE = "dvlin";
 export const SITE_DESCRIPTION =
-  "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
+  "dvlin is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
 
 export const MENUS = [
   {
